927bb4640b7ba624b58b9c5a9c8764b775a62714
FreshDev <billyhans90@gmail.com>
2023-12-31 10:33:50 +0100

    Solve validation bug

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ const User = mongoose.model('User', userSchema)
 // Validation rules using Joi
 function validateUser(user) {
     const schema = Joi.object({
-        name: Joi.string().min(5).max(50).required(),
+        user_name: Joi.string().min(5).max(50).required(),
         join_date: Joi.date()
     })
     return schema.validate(user)
