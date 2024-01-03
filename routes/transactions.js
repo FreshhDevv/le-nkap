af3d636106ad04b2de31c3d63f70906fdefd35fc
FreshDev <billyhans90@gmail.com>
2024-01-03 16:50:52 +0100

    Get and update transaction

--- routes/transactions.js
+++ routes/transactions.js
@@ -28,4 +28,26 @@ router.post('/', async(req, res) => {
     res.send(transaction)
 })
 
+router.get('/:id', async (req, res) => {
+    const transaction = await Transaction.findById(req.params.id)
+    if(!transaction) return res.status(404).send('The transaction with the given ID was not found.')
+    res.send(transaction)
+})
+
+router.put('/:id', async (req, res) => {
+    const {error} = validate(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+    const category = await Category.findById(req.body.categoryId)
+    if(!category) return res.status(404).send('Invalid category')
+
+    const transaction = await Transaction.findByIdAndUpdate(
+        req.params.id,
+        {name: req.body.name, type: req.body.type, amount: req.body.amount, category: {_id: category._id, name: category.name}},
+        {new: true}
+)
+if(!transaction) return res.status(404).send('The transaction with the given ID was not found.')
+res.send(transaction)
+})
+
 module.exports = router
