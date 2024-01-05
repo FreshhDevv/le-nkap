7594af74a919e61ad41125ff13c8165a5448a5a6
FreshDev <billyhans90@gmail.com>
2024-01-05 03:42:16 +0100

    Refactor

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
+const auth = require('../middleware/auth')
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
-router.get("/", async (req, res) => {
-  const users = await User.find().sort("name");
-  res.send(users);
-});
+// router.get("/", async (req, res) => {
+//   const users = await User.find().sort("name");
+//   res.send(users);
+// });
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
@@ -28,31 +29,30 @@ router.post("/", async (req, res) => {
     .send(_.pick(user, ["_id", "name", "email"]));
 });
 
-router.get("/:id", async (req, res) => {
-  const user = await User.findById(req.params.id);
-  if (!user)
-    return res.status(404).send("The user with the given ID was not found.");
+// Get logged in user
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id);
   res.send(user);
 });
 
-router.put("/:id", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  const user = await User.findByIdAndUpdate(
-    req.params.id,
-    { name: req.body.name, join_date: req.body.join_date },
-    { new: true }
-  );
-  if (!user)
-    return res.status(404).send("The user with the given ID was not found");
-  res.send(user);
-});
+// router.put("/:id", async (req, res) => {
+//   const { error } = validate(req.body);
+//   if (error) return res.status(400).send(error.details[0].message);
+//   const user = await User.findByIdAndUpdate(
+//     req.params.id,
+//     { name: req.body.name, join_date: req.body.join_date },
+//     { new: true }
+//   );
+//   if (!user)
+//     return res.status(404).send("The user with the given ID was not found");
+//   res.send(user);
+// });
 
-router.delete("/:id", async (req, res) => {
-  const user = await User.findByIdAndDelete(req.params.id);
-  if (!user)
-    return res.status(404).send("The user with the given ID was not found.");
-  res.send(user);
-});
+// router.delete("/:id", async (req, res) => {
+//   const user = await User.findByIdAndDelete(req.params.id);
+//   if (!user)
+//     return res.status(404).send("The user with the given ID was not found.");
+//   res.send(user);
+// });
 
 module.exports = router;
