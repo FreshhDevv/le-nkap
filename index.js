3e88ae790b3f0dca49316fb29b0919191307e9d3
FreshDev <billyhans90@gmail.com>
2024-01-06 11:46:59 +0100

    Go back to mongo db

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if(!config.get('jwtPrivateKey')) {
 }
 
 mongoose
-  .connect(process.env.MONGODB)
+  .connect(process.env.MONGODB_CONNECT_URI)
   .then(() => console.log("Connected to MongoDb..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
