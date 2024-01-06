a975700b94ac4fd3f909140a2c9ac03509fcea0b
FreshDev <billyhans90@gmail.com>
2024-01-06 10:09:18 +0100

    Fix documentation

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if(!config.get('jwtPrivateKey')) {
 }
 
 mongoose
-  .connect(process.env.MONGODB_CONNECT_URI)
+  .connect(process.env.MONGODB)
   .then(() => console.log("Connected to MongoDb..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,5 @@
-const { addCategory } = require("../controllers/categoryController");
-const { getCategories } = require("../controllers/categoryController");
+const { addCategory } = require("../controllers/categoryController.js");
+const { getCategories } = require("../controllers/categoryController.js");
 const auth = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
