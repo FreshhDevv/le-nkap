d532dc66900971498b4b02152dea8e63e76cae78
FreshDev <billyhans90@gmail.com>
2024-01-07 20:22:41 +0100

    Handling rejected promises

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,8 +2,13 @@ const { Category, validate } = require("../models/category");
 
 const getCategories = async (req, res) => {
   const userId = req.user._id;
-  const categories = await Category.find({ userId: userId }).sort("name");
-  res.send(categories);
+  try{
+    const categories = await Category.find({ userId: userId }).sort("name");
+    res.send(categories);
+  }
+  catch(ex) {
+    res.status(500).send('Something failed.')
+  }
 };
 
 const addCategory = async (req, res) => {
