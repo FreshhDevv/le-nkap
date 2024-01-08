2ae0d70fcecf6acfbd1d258ea7853a9f5388c8e6
FreshDev <billyhans90@gmail.com>
2024-01-09 00:47:27 +0100

    Refactor the auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,31 +2,31 @@ const _ = require("lodash");
 const { User } = require("../models/user");
 const Joi = require("joi");
 const bcrypt = require("bcrypt");
+const asyncMiddleware = require("../middleware/async");
 
+const login = asyncMiddleware(async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-const login = async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-  
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send("Invalid email or password");
-  
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send("Invalid email or password");
-  
-    const token = user.generateAuthToken();
-    res.send(token);
-  }
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password");
 
-  // Validate the email and password gotten from the user.
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password");
+
+  const token = user.generateAuthToken();
+  res.send(token);
+});
+
+// Validate the email and password gotten from the user.
 function validate(req) {
-    const schema = Joi.object({
-      email: Joi.string().min(5).max(255).required().email(),
-      password: Joi.string().min(8).max(255).required(),
-    });
-    return schema.validate(req);
-  }
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(8).max(255).required(),
+  });
+  return schema.validate(req);
+}
 
-  module.exports = {
-    login,
-  };
+module.exports = {
+  login,
+};
