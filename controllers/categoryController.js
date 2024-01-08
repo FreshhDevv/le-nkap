8f2d267d9ccd87c6fa99da7234f94c31bd6140d5
FreshDev <billyhans90@gmail.com>
2024-01-09 00:48:36 +0100

    Refactor the category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,17 +1,17 @@
+const asyncMiddleware = require("../middleware/async");
 const { Category, validate } = require("../models/category");
 
-const getCategories = async (req, res, next) => {
+const getCategories = asyncMiddleware(async (req, res, next) => {
   const userId = req.user._id;
-  try{
+  try {
     const categories = await Category.find({ userId: userId }).sort("name");
     res.send(categories);
+  } catch (ex) {
+    next(ex);
   }
-  catch(ex) {
-    next(ex)
-  }
-};
+});
 
-const addCategory = async (req, res) => {
+const addCategory = asyncMiddleware(async (req, res) => {
   const { error } = validate(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
@@ -24,7 +24,7 @@ const addCategory = async (req, res) => {
   });
   await category.save();
   res.send(category);
-};
+});
 
 module.exports = {
   getCategories,
